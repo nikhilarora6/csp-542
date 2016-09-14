b3c60539fc770c4d04ffbc7c54ebab0e406ea63f
NIK ARORA <NIK ARORA>
2016-09-13 20:42:31 -0500

    cs542 project

---
+++ csp-542/src/cs542/CS542.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs542;
+
+/**
+ *
+ * @author NIK
+ */
+public class CS542 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ProgressBar().setVisible(true);
+    }
+    
+}
